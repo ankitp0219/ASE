aba6b57e49623c35cfd32990f70812fc4847d738
ankitp0219 <67831868+ankitp0219@users.noreply.github.com>
2023-01-01 16:39:16 +0530

    A S ENTERPRISES

---
+++ asset/mail/contact.php
@@ -0,0 +1,52 @@
+<!-- <?php
+if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+  http_response_code(500);
+  exit();
+}
+
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email = strip_tags(htmlspecialchars($_POST['email']));
+$m_subject = strip_tags(htmlspecialchars($_POST['subject']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+
+$to = "asenterprises.mgps@gmail.com"; // Change this email to your //
+$subject = "$m_subject:  $name";
+$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
+$header = "From: $email";
+$header .= "Reply-To: $email";	
+
+if(!mail($to, $subject, $body, $header))
+  http_response_code(500);
+?> --> 
+
+
+<?php
+
+function strip_crlf($string)
+{
+    return str_replace("\r\n", "", $string);
+}
+
+if (! empty($_POST["send"])) {
+    $name = $_POST["userName"];
+    $email = $_POST["userEmail"];
+    $subject = $_POST["subject"];
+    $content = $_POST["content"];
+
+    $toEmail = "admin@phppot_samples.com";
+    // CRLF Injection attack protection
+    $name = strip_crlf($name);
+    $email = strip_crlf($email);
+    if (! filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "The email address is invalid.";
+    } else {
+        // appending \r\n at the end of mailheaders for end
+        $mailHeaders = "From: " . $name . "<" . $email . ">\r\n";
+        if (mail($toEmail, $subject, $content, $mailHeaders)) {
+            $message = "Your contact information is received successfully.";
+            $type = "success";
+        }
+    }
+}
+require_once "contact-view.php";
+?>
